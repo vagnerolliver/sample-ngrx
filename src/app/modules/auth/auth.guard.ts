19e220e0cef4532606e067e54f7e2b47009c9bc9
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2019-01-13 02:29:10 -0200

    Effects auth (#4)
    * auth effects
    
    * authEffects refresh page
    
    * Merge pull request #1 from vagnerolliver2/effects-auth (#2)
    
    * Merge pull request #1 from vagnerolliver2/effects-auth (#3)

--- src/app/modules/auth/auth.guard.ts
+++ src/app/modules/auth/auth.guard.ts
@@ -25,12 +25,11 @@ export class AuthGuard implements CanActivate {
     return this.store
       .pipe(
         select(isLoggedIn),
-          tap(loggedIn => {
-            if (!loggedIn) {
-              this.router.navigateByUrl('/login');
-            }
+        tap(loggedIn => {
+          if (!loggedIn) {
+           // this.router.navigateByUrl('/login');
           }
-        )
+        })
       );
   }
 }
